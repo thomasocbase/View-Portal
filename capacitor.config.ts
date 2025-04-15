390231afc9512f48ace81d260e3ed2f133282c2c
Thomas Dutouquet <thomas@ocbase.com>
2025-04-15 09:29:24 +0200

    Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'Micro Portal',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    cleartext: true,
+    androidScheme: 'https',
+  }
 };
 
 export default config;
